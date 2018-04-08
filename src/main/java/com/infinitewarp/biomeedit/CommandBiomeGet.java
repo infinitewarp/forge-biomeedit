7fdaa3fe32364e0b575ccc7fe2b4221d09ef0d7c
Brad Smith <bradster@infinitewarp.com>
2018-04-07 23:05:55 -0400

    first prototype to get and set biome at coordinate

---
+++ src/main/java/com/infinitewarp/biomeedit/CommandBiomeGet.java
@@ -0,0 +1,66 @@
+package com.infinitewarp.biomeedit;
+
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommandBiomeGet implements ICommand {
+
+    @Override
+    public String getName() {
+        return "biomeget";
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return "/biomeget <z> <x> displays biome info for coordinate (x,z)";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Arrays.asList(new String[] { });
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        World world = sender.getEntityWorld();
+        int x = new Integer(args[0]);
+        int z = new Integer(args[1]);
+        BlockPos pos = new BlockPos(x, 1, z);
+
+//        TODO why is getBiomeName broken? It throws java.lang.NoSuchMethodError: net.minecraft.world.biome.Biome.getBiomeName()Ljava/lang/String
+//        String biomeName = world.getChunkFromBlockCoords(pos).getBiome(pos, world.getBiomeProvider()).getBiomeName();
+        Biome biome = world.getChunkFromBlockCoords(pos).getBiome(pos, world.getBiomeProvider());
+        String biomeName = String.valueOf(Biome.REGISTRY.getNameForObject(biome));
+        sender.sendMessage(new TextComponentString(String.format("biome at (%s, %s) is %s", x, z, biomeName)));
+    }
+
+    @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
+        return null;
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return false;
+    }
+
+    @Override
+    public int compareTo(ICommand o) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/infinitewarp/biomeedit/CommandBiomeSet.java
@@ -0,0 +1,75 @@
+package com.infinitewarp.biomeedit;
+
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommandBiomeSet implements ICommand {
+
+    @Override
+    public String getName() {
+        return "biomeset";
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return "/biomeset <z> <x> <biomename> set specified biome at coordinate (x,z)";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Arrays.asList(new String[]{});
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        World world = sender.getEntityWorld();
+        int x = Integer.parseInt(args[0]);
+        int z = Integer.parseInt(args[1]);
+        BlockPos pos = new BlockPos(x, 1, z);
+
+        Chunk chunk = world.getChunkFromBlockCoords(pos);
+        byte[] biomeArray = chunk.getBiomeArray();
+        int biomeArrayPos = (z & 15) << 4 | (x & 15);
+
+        Biome newBiome = Biome.REGISTRY.getObject(new ResourceLocation(args[2]));
+        String newbiomeName = String.valueOf(Biome.REGISTRY.getNameForObject(newBiome));
+
+        biomeArray[biomeArrayPos] = (byte) Biome.REGISTRY.getIDForObject(newBiome);
+        chunk.setBiomeArray(biomeArray);
+        chunk.markDirty();
+
+        sender.sendMessage(new TextComponentString(String.format("biome at (%s, %s) is now %s", x, z, newbiomeName)));
+    }
+
+    @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
+        return null;
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return false;
+    }
+
+    @Override
+    public int compareTo(ICommand o) {
+        return 0;
+    }
+}
