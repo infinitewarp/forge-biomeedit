d9ff4eaddc3b8006d879aec197f858b5b508d421
Brad Smith <bradster@infinitewarp.com>
2018-04-08 19:45:51 -0400

    clean up build.gradle

--- src/main/java/com/infinitewarp/biomeedit/BiomeEditMod.java
+++ src/main/java/com/infinitewarp/biomeedit/BiomeEditMod.java
@@ -19,7 +19,7 @@ public class BiomeEditMod
 {
     public static final String MODID = "biomeedit";
     public static final String NAME = "BiomeEdit";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "%VERSION%";
 
     private static Logger logger;
 
